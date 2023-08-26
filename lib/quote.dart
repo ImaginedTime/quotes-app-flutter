41a37ed85b9fc79b9d61ae62186180178e1054d8
Uday <udayomsrivastava111@gmail.com>
2023-08-26 12:36:46 +0530

    making the quotes app

---
+++ lib/quote.dart
@@ -0,0 +1,9 @@
+class Quote {
+
+  late String text;
+  late String author;
+
+  Quote({ required this.text, required this.author });
+}
+
+Quote myquote = Quote(text : "this is the quote text", author: "oscar wilde");
---
+++ lib/quote_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+
+  final Quote quote;
+  final void Function() delete;
+  QuoteCard({ required this.quote , required this.delete});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+    margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+    child: Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            quote.text,
+            style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600]
+            ),
+          ),
+
+          SizedBox(height: 6.0,),
+
+          Text(
+            "– ${quote.author}",
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.grey[800],
+            ),
+          ),
+
+          SizedBox(height: 8.0,),
+
+          TextButton.icon(
+            onPressed: delete,
+            label: Text("delete Quote"),
+            icon: Icon(Icons.delete),
+          )
+        ],
+      ),
+    ),
+  );
+  }
+}
